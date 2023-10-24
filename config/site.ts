9e33b4929e669d5483738cc08476253089facc8a
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2023-10-24 19:59:44 +0100

    shadcn template

---
+++ config/site.ts
@@ -0,0 +1,18 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Nermalcat69",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [
+    {
+      title: "Exif Data Viewer",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/nermalcat69",
+    github: "https://github.com/nermalcat69",
+    docs: "https://ui.shadcn.com",
+  },
+}
