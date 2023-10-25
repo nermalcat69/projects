aa5d88cb65f587796ade1b71e3c15b3e2f4b13b7
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2023-10-25 06:31:24 +0100

    prettier

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -13,9 +13,13 @@ interface MainNavProps {
 export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-4 md:gap-2">
-      <Link href="https://www.nermalcat69.com" className="flex items-center space-x-2">
+      <Link
+        href="https://www.nermalcat69.com"
+        className="flex items-center space-x-2"
+      >
         <Icons.logo className="h-6 w-6" />
-        <span className="inline-block font-bold">{siteConfig.name}</span><span>/</span>
+        <span className="inline-block font-bold">{siteConfig.name}</span>
+        <span>/</span>
       </Link>
       {items?.length ? (
         <nav className="flex gap-6">
