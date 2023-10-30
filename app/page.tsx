5eeb3d8c56ef16f44cda0aba163916e768bf9bc6
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2023-10-30 15:45:00 +0000

    fixed a text

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tight md:text-4xl">
+        <h1 className="text-3xl font-bold leading-tight tracking-normal md:text-4xl">
           Projects
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
