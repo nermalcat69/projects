db79a3380dd6fad54ac4060bf0890d0917bec50a
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2023-10-31 09:51:33 +0000

    (testing stuff)card changes

--- components/globals/card.tsx
+++ components/globals/card.tsx
@@ -5,7 +5,7 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ releaseDate, name, link }) => (
-  <div className="card py-8 px-5">
+  <div className="card py-4 px-3 md:py-8 md:px-5">
     <a href={link} className="card-link">
       <h2 className="text-md md:text-lg font-bold text-gray-800">{name}</h2>
       <span className="text-gray-600">{releaseDate}</span>
--- components/projects.tsx
+++ components/projects.tsx
@@ -6,13 +6,21 @@ const projectsData = [
   { releaseDate: "2023", name: "Test", link: "https://google.com" },
   { releaseDate: "2022", name: "test 2", link: "https://google.com" },
   { releaseDate: "2021", name: "LOLZ", link: "https://google.com" },
+    { releaseDate: "2021", name: "LOLZ", link: "https://google.com" },
+  { releaseDate: "2021", name: "LOLZ", link: "https://google.com" },
+  { releaseDate: "2021", name: "LOLZ", link: "https://google.com" },
+  { releaseDate: "2021", name: "LOLZ", link: "https://google.com" },
+  { releaseDate: "2021", name: "LOLZ", link: "https://google.com" },
+  { releaseDate: "2021", name: "LOLZ", link: "https://google.com" },
+  { releaseDate: "2021", name: "LOLZ", link: "https://google.com" },
+
 ]
 
 export default function Projects() {
   return (
     <div className="py-2 pb-10">
       <div className="m-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 content-around ">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 content-around ">
           {projectsData.map((project, index) => (
             <Card key={index} {...project} />
           ))}
