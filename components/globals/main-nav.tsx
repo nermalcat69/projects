6041826549c5a489eec60fae9e8ddb3c56fa0df6
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2023-10-31 09:26:25 +0000

    link update in main-nav.tsx

--- components/globals/main-nav.tsx
+++ components/globals/main-nav.tsx
@@ -14,7 +14,7 @@ interface MainNavProps {
 export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex items-center gap-4 md:gap-2">
-      <Link href="https://www.nermalcat69.com" className="flex space-x-2 items-center">
+      <Link href="/" className="flex space-x-2 items-center">
         <span className="pr-1"><Gradient /></span>
         <span className="text-xl text-gray-800 inline-block font-bold">
           {siteConfig.name}
