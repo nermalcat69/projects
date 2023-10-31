a0278be0f7762e13db57c3d357004fcc7d1d6fc1
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2023-10-31 10:01:39 +0000

    card design

--- components/globals/card.tsx
+++ components/globals/card.tsx
@@ -6,8 +6,8 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ releaseDate, name, link }) => (
   <div className="card py-4 px-3 md:py-8 md:px-5">
-    <a href={link} className="card-link">
-      <h2 className="text-md md:text-lg font-bold text-gray-800">{name}</h2>
+    <a href={link}>
+      <h2 className="text-md md:text-lg font-bold  text-gray-800">{name}</h2>
       <span className="text-gray-600">{releaseDate}</span>
     </a>
   </div>
