1995f17ebd275ba41be2865ee1147edaa1d01161
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2023-11-11 16:48:51 +0000

    lines svg

--- components/svgs/lines.tsx
+++ components/svgs/lines.tsx
@@ -6,54 +6,54 @@ export default function Lines() {
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <g clip-path="url(#clip0_6_2)">
+        <g clipPath="url(#clip0_6_2)">
           <path
             d="M766 141H875.116H1100"
             stroke="#CECECE"
-            stroke-width="0.5"
-            stroke-dasharray="5 5"
+            strokeWidth="0.5"
+            strokeDasharray="5 5"
           />
           <path
             d="M0 137H109.116H334"
             stroke="url(#paint0_linear_6_2)"
-            stroke-width="0.5"
-            stroke-dasharray="5 5"
+            strokeWidth="0.5"
+            strokeDasharray="5 5"
           />
           <path
             d="M0 93H109.116H334"
             stroke="#CECECE"
-            stroke-width="0.5"
-            stroke-dasharray="5 5"
+            strokeWidth="0.5"
+            strokeDasharray="5 5"
           />
           <path
             d="M0 115H109.116H334"
             stroke="#CECECE"
-            stroke-width="0.5"
-            stroke-dasharray="5 5"
+            strokeWidth="0.5"
+            strokeDasharray="5 5"
           />
           <path
             d="M0 93H109.116H334"
             stroke="url(#paint1_linear_6_2)"
-            stroke-width="0.5"
-            stroke-dasharray="5 5"
+            strokeWidth="0.5"
+            strokeDasharray="5 5"
           />
           <path
             d="M0 115H109.116H334"
             stroke="url(#paint2_linear_6_2)"
-            stroke-width="0.5"
-            stroke-dasharray="5 5"
+            strokeWidth="0.5"
+            strokeDasharray="5 5"
           />
           <path
             d="M766 141H875.116H1100"
             stroke="url(#paint3_linear_6_2)"
-            stroke-width="0.5"
-            stroke-dasharray="5 5"
+            strokeWidth="0.5"
+            strokeDasharray="5 5"
           />
           <path
             d="M871 227.241L927.597 170.644L1044.24 54"
             stroke="#CECECE"
-            stroke-width="0.5"
-            stroke-dasharray="5 5"
+            strokeWidth="0.5"
+            strokeDasharray="5 5"
           />
         </g>
         <defs>
@@ -65,8 +65,8 @@ export default function Lines() {
             y2="137"
             gradientUnits="userSpaceOnUse"
           >
-            <stop stop-color="#F2F2F2" />
-            <stop offset="1" stop-color="#CECECE" />
+            <stop stopColor="#F2F2F2" />
+            <stop offset="1" stopColor="#CECECE" />
           </linearGradient>
           <linearGradient
             id="paint1_linear_6_2"
@@ -76,8 +76,8 @@ export default function Lines() {
             y2="93"
             gradientUnits="userSpaceOnUse"
           >
-            <stop stop-color="#CECECE" />
-            <stop offset="1" stop-color="#F2F2F2" />
+            <stop stopColor="#CECECE" />
+            <stop offset="1" stopColor="#F2F2F2" />
           </linearGradient>
           <linearGradient
             id="paint2_linear_6_2"
@@ -87,8 +87,8 @@ export default function Lines() {
             y2="115"
             gradientUnits="userSpaceOnUse"
           >
-            <stop stop-color="#F2F2F2" />
-            <stop offset="1" stop-color="#CECECE" />
+            <stop stopColor="#F2F2F2" />
+            <stop offset="1" stopColor="#CECECE" />
           </linearGradient>
           <linearGradient
             id="paint3_linear_6_2"
@@ -98,8 +98,8 @@ export default function Lines() {
             y2="141"
             gradientUnits="userSpaceOnUse"
           >
-            <stop stop-color="#CECECE" />
-            <stop offset="1" stop-color="#F2F2F2" />
+            <stop stopColor="#CECECE" />
+            <stop offset="1" stopColor="#F2F2F2" />
           </linearGradient>
           <clipPath id="clip0_6_2">
             <rect width="1100" height="230" fill="white" />
